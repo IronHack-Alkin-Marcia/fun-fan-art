d7faf42731675ce95c3b5fc9036e734fd71a62f4
Marcia Faria <mfndnc@gmail.com>
2021-04-26 11:34:42 +0200

    npm install passport and co - basic auth

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require('dotenv/config');
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express');
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
+const hbs = require('hbs');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require("./config")(app);
+require('./config')(app);
+
+// move all passports configuration to /config/
+// **** using one file per Strategy to make it more readable
+require('./config/passport.local')(app);
+require('./config/passport')(app);
+require('./config/passport.global')(app);
 
 // default value for title local
-const projectName = "fun-fan-art";
+const projectName = 'fun-fan-art';
 const capitalized = (string) =>
   string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)} created with Ironlauncher`;
 
 // 👇 Start handling routes here
-const index = require("./routes/index");
-app.use("/", index);
+const index = require('./routes/index');
+app.use('/', index);
+
+app.use('/', require('./routes/auth'));
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
