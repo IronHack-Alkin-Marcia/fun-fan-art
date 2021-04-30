b3c4e8f4135ccd4e8850c8544ddeb12438c8567a
Marcia Faria <mfndnc@gmail.com>
2021-04-30 08:47:12 +0200

    showing tag in title

--- routes/bookmark.js
+++ routes/bookmark.js
@@ -6,10 +6,29 @@ const BookMark = require('../models/BookMark');
 const { loginCheck } = require('./middlewares');
 
 router.get('/', loginCheck(), (req, res, next) => {
-  BookMark.find({ user: req.user._id })
+  Promise.all([
+    BookMark.find({ user: req.user._id })
+      .distinct('tag')
+      .then((tags) => tags),
+    BookMark.find({ user: req.user._id })
+      .populate('art')
+      .then((bookmarks) => bookmarks),
+  ])
+    .then(([tags, bookmarks]) => res.render('bookmark/', { bookmarks, tags }))
+    .catch((err) => next(err));
+});
+
+router.get('/tags/:tag', loginCheck(), (req, res, next) => {
+  let queryval = req.params.tag == '0' ? null : req.params.tag;
+  let subtitle =
+    req.params.tag == '0'
+      ? 'Your art not tagged'
+      : `Your art tagged as "${req.params.tag}"`;
+
+  BookMark.find({ user: req.user._id, tag: queryval })
     .populate('art')
-    .then((bookmark) => res.render('bookmark/', { bookmark }))
-    .catch((error) => next(error));
+    .then((bookmarks) => res.render('bookmark/tagged', { bookmarks, subtitle }))
+    .catch((err) => next(err));
 });
 
 router.get('/:id', loginCheck(), (req, res, next) => {
